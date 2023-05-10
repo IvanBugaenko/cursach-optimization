3e6ca4614b5d302be0c0b7389644fd50a00c2b82
melskiy <87464481+melskiy@users.noreply.github.com>
2023-05-10 21:09:44 +0600

    add settings

--- client/front/src/input.js
+++ client/front/src/input.js
@@ -1,67 +1,46 @@
 import React, { useState } from "react";
 import QueenPuzzle from "./Doska";
-import Slider from "@mui/material/Slider";
+
+import { IconButton} from '@mui/material';
+import TuneIcon from '@mui/icons-material/Tune';
+import Settings from "./settings";
 
 function NumberInput() {
   const [value, setValue] = useState("");
-  const [valueMu, setValueMu] = useState("");
-  const [valueSk, setValueSk] = useState("");
+ 
   const [size, setSize] = useState();
   const [displayQueenPuzzle, setDisplayQueenPuzzle] = useState(false);
-
+  const [displaySettings, setDisplaySettings] = useState(false);
+  let datas;
+  const handleChildData = (data) => {
+    datas = data;
+  };
+ 
   const handleChange = (event) => {
     const newValue = event.target.value;
     setValue(newValue);
     setDisplayQueenPuzzle(false);
   };
-  const handleChangeSlide = (event, newValue) => {
-    setValueMu(newValue);
-  };
-
-  const handleChangeSlide2 = (event, newValue) => {
-    setValueSk(newValue);
-  };
+ 
   const handleSubmit = (event) => {
     event.preventDefault();
     setSize(value);
+
+    
     setDisplayQueenPuzzle(true);
   };
+
+  const setSettings = () =>{
+    setDisplaySettings(!displaySettings);
+  }
+ 
   return (
     <div>
-      <div>
-        <p>Вероятность мутации</p>
-        <Slider
-          value={valueMu}
-          onChange={handleChangeSlide}
-          step={0.01}
-          min={0}
-          max={1}
-          valueLabelDisplay="auto"
-          sx={{
-            width: 300,
-            color: "gray",
-          }}
-        />
-      </div>
-
-      <div>
-        <p>Вероятность скрещивания</p>
-        <Slider
-          value={valueSk}
-          onChange={handleChangeSlide2}
-          step={0.01}
-          valueLabelDisplay="auto"
-          min={0}
-          max={1}
-          sx={{
-            width: 300,
-            color: "gray",
-          }}
-        />
-      </div>
+      <h2>Задача о 8 ферзях <IconButton onClick={setSettings} ><TuneIcon  variant="contained" /> </IconButton> </h2>
+      {displaySettings && <Settings onData={handleChildData} />}
       <form onSubmit={handleSubmit}>
         <input type="text" value={value} onChange={handleChange} />
-        <button type="submit">Submit</button>
+        <button type="submit" className="send">Submit</button>
       </form>
       {displayQueenPuzzle && <QueenPuzzle size={size} />}
     </div>
---
+++ client/front/src/settings.js
@@ -0,0 +1,97 @@
+
+import React ,{ useState }from 'react';
+import Slider from "@mui/material/Slider";
+import TextField from '@mui/material/TextField';
+
+function Settings(props) {
+const [valueMu, setValueMu] = useState("");
+const [valueSk, setValueSk] = useState(""); 
+
+let dictionary = {
+    N: '8',
+    population_dim: '100',
+    tournament_size:"5",
+    crossing_probability:"0.85",
+    mutation_probability: "0.15",
+    exchange_num: "3",
+    steps: "100"
+  };
+const handleChangePop = (event, newValue) => {
+    dictionary["population_dim"] = event.target.value;
+    props.onData(dictionary);
+  };
+
+  const handleChangeStep = (event, newValue) => {
+    
+    dictionary["steps"] = event.target.value;
+  };
+
+  const handleChangeValueMut = (event, newValue) => {
+   
+    dictionary["exchange_num"] = event.target.value;
+  };
+
+  const handleChangeSize = (event, newValue) => {
+    
+    dictionary["tournament_size"] = event.target.value;
+  };
+
+  const handleChangeSlide = (event, newValue) => {
+    setValueMu(newValue);
+    dictionary["mutation_probability"] = newValue;
+  };
+  const handleChangeSlide2 = (event, newValue) => {
+    setValueSk(newValue);
+    dictionary["crossing_probability"] = newValue;
+    console.log(dictionary)
+  };
+    
+ return (
+    <div>
+    <div>
+        <div>
+    <TextField size = "small" defaultValue = {dictionary["population_dim"]} id="standard-basic" label="Размер популяци"variant="standard" onChange={handleChangePop} />
+    <TextField size = "small" defaultValue = {dictionary["tournament_size"]} id="standard-basic" label="Размер Турнира"variant="standard" onChange={ handleChangeSize} />
+    <br></br>
+    <TextField size = "small" defaultValue = {dictionary["steps"]}  id="standard-basic" label="Число шагов"variant="standard"  onChange={handleChangeStep}/>
+    <TextField size = "small" defaultValue = {dictionary["exchange_num"]}  id="standard-basic" label="Количество мутаций "variant="standard" onChange={handleChangeValueMut} />
+    </div>
+        <p>Вероятность мутации </p>
+        <Slider
+          value={valueMu}
+          onChange={handleChangeSlide}
+          step={0.01}
+          min={0}
+          max={1}
+          valueLabelDisplay="auto"
+          sx={{
+            width: 300,
+            color: "gray",
+          }}
+        />
+      </div>
+
+      <div>
+        <p>Вероятность скрещивания</p>
+        <Slider
+          value={valueSk}
+          onChange={handleChangeSlide2}
+          step={0.01}
+          valueLabelDisplay="auto"
+          min={0}
+          max={1}
+          sx={{
+            width: 300,
+            color: "gray",
+          }}
+        />
+      </div>
+
+      <div>
+      
+      </div>
+      </div>
+    );
+  }
+  
+  export default Settings;
